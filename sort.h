9a14363065700158af608ec81ad5fb7a094b9495
Abokiey <sweneysam@gmail.com>
2023-09-22 12:27:36 +0300

    bubble sort

--- sort.h
+++ sort.h
@@ -20,7 +20,7 @@ typedef struct listint_s
 
 /*prototypes*/
 void print_array(const int *array, size_t size);
-void print_li st(const listint_t *list);
+void print_list(const listint_t *list);
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
@@ -33,7 +33,7 @@ void heap_sort(int *array, size_t size);
 void radix_sort(int *array, size_t size);
 void bitonic_sort(int *array, size_t size);
 void quick_sort_hoare(int *array, size_t size);
-void sort_deck(deck_node_t **deck);
+/*void sort_deck(deck_node_t **deck);*/
 
 
 
